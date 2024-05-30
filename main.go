24ded03842b47bd02054d9d50a55e1ab50e45718
4rMyy <56570563+starmkodinw@users.noreply.github.com>
2024-05-30 23:25:47 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
-import "dca-bitcoin/services"
+import (
+	"dca-bitcoin/services"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	services.BuyBitcoin()
 	go services.StartCronJob()
-	select {}
+
+	port := 8080
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world"))
+	})
+
+	err := http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
